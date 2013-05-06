4e4011142092bf89aef1ec6180ca4d7fd8b31853
John White <jonttyy@gmail.com>
2013-05-06 21:33:57 +0200

    Fix style.css path - closes #4

--- views/indexView.php
+++ views/indexView.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<link rel="stylesheet" href="style.css"/>
+		<link rel="stylesheet" href="/lib/css/style.css"/>
 		<title><?php echo $title;?></title>
 	</head>
 	<body>
